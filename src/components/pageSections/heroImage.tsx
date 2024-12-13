8d81a1160af3c3463f95f955f55c0c4f2417b7b8
Valesca Sarrocchi <valescasarrocchi@hotmail.com>
2024-12-13 14:13:16 +0100

    improve accessibility

--- src/components/pageSections/heroImage.tsx
+++ src/components/pageSections/heroImage.tsx
@@ -2,9 +2,9 @@ import ResponsiveImage from "@/components/responsiveImage";
 import { SanityImageIn } from "@/types";
 
 const HeroImage: React.FC<{ image: SanityImageIn }> = ({ image }) => (
-  <section className="relative aspect-video w-full max-h-[524px]">
+  <div className="relative aspect-video w-full max-h-[524px]">
     <ResponsiveImage image={image} />
-  </section>
+  </div>
 );
 
 export default HeroImage;
