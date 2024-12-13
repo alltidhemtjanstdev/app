8500f0f4a353fedcd3f12e608dbbd9a364134d4a
Valesca Sarrocchi <valescasarrocchi@hotmail.com>
2024-12-13 15:15:57 +0100

    update accessibility text

--- src/pages/tillganglighetsredogorelse.tsx
+++ src/pages/tillganglighetsredogorelse.tsx
@@ -28,6 +28,34 @@ const AccessibilityStatement = () => {
           </p>
         </div>
         <div className="mb-10">
+          <h2>Kontrastproblem för Call-to-Action (CTA)-knappen</h2>
+          <p>
+            Vi har upptäckt att kontrasten mellan bakgrundsfärgen och texten på
+            vår CTA-knapp inte uppfyller WCAG 2.1-standarderna för tillräcklig
+            kontrast. Detta kan göra det svårt för vissa användare att läsa och
+            interagera med knappen. Vi arbetar aktivt på att åtgärda detta
+            problem och kommer att uppdatera designen för att säkerställa att
+            den uppfyller alla krav på tillgänglighet.
+          </p>
+        </div>
+        <div className="mb-10 flex flex-col gap-4">
+          <h2>Oskäligt betungande anpassning</h2>
+          <p>
+            Alltid Hemtjänst och Service hänvisar till undantaget för oskäligt
+            betungande anpassning enligt 12 § i lagen om tillgänglighet till
+            digital offentlig service för:
+          </p>
+          <ul className="list-disc ml-6">
+            <li>Brukarundersökningar</li>
+            <li>Uppföljningsrapporter</li>
+          </ul>
+          <p>
+            Att göra dessa dokument tillgängliga kräver ett mycket omfattande
+            arbete. Vi strävar istället efter att presentera innehållet på ett
+            mer tillgängligt och användarvänligt sätt än i den nuvarande formen.
+          </p>
+        </div>
+        <div className="mb-10">
           <h2>Hur du kontaktar oss</h2>
           <p>
             Om du upplever några svårigheter med att komma åt innehållet på
